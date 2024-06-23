b3598a21c3ea982a92907b57269808aa31f1126c
Antonio Fernandez Naharro <antofern@c1r10s5.42madrid.com>
2024-06-23 18:35:31 +0200

    aclaraciones en readme

--- jp_tester.c
+++ jp_tester.c
@@ -10,7 +10,6 @@ int main(void)
     while(i <= 20) //vamos a suponer que la funcion que estamos testeando, llamara a malloc como maximo 20 veces.
     {
         variable_global = i;
-        //printf("\n-------llamada numero %d de funcion_testeada()--------", i);
         retorno_error = funcion_testeada();
         if( retorno_error == 0)
             printf("--------fallo en malloc n%d no manejado------\n",i);
